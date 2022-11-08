3899b734949ffb154feb1687e9b769f1890a8406
Fungi 3 <sylvienuijtemans@gmail.com>
2022-11-08 14:10:43 +0100

    js audios

---
+++ module-3/workshop-1/assets/js/script.js
@@ -0,0 +1,39 @@
+function toggleButtercupAudio() {
+    var buttercupAudio = document.getElementById('buttercup-audio');
+    if (buttercupAudio.paused)
+        buttercupAudio.play();
+    else
+        buttercupAudio.pause();
+}
+
+function toggleSoftboyAudio() {
+    var softboyAudio = document.getElementById('soft-boy-audio');
+    if (softboyAudio.paused)
+        softboyAudio.play();
+    else
+        softboyAudio.pause();
+}
+
+function toggleVioletAudio() {
+    var violetAudio = document.getElementById('violet-audio');
+    if (violetAudio.paused)
+        violetAudio.play();
+    else
+        violetAudio.pause();
+}
+
+function toggleSortingalgorithmsAudio() {
+    var sortingalgorithmsAudio = document.getElementById('sorting-algorithms-audio');
+    if (sortingalgorithmsAudio.paused)
+        sortingalgorithmsAudio.play();
+    else
+        sortingalgorithmsAudio.pause();
+}
+
+function toggleLittledarkageAudio() {
+    var littledarkageAudio = document.getElementById('little-dark-audio');
+    if (littledarkageAudio.paused)
+        littledarkageAudio.play();
+    else
+        littledarkageAudio.pause();
+}
