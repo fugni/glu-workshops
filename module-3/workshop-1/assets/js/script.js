ca7633dc6261c56d14bc63b2ab2334150e915071
Fungi 3 <sylvienuijtemans@gmail.com>
2022-11-10 08:49:44 +0100

    audios

--- module-3/workshop-1/assets/js/script.js
+++ module-3/workshop-1/assets/js/script.js
@@ -1,29 +1,60 @@
+var audio1 = document.getElementById("buttercup-audio");
+var audio2 = document.getElementById("soft-boy-audio");
+var audio3 = document.getElementById("violet-audio");
+var audio4 = document.getElementById("sorting-algorithms-audio");
+var audio5 = document.getElementById("little-dark-age-audio");
+
 function toggleAudio1() {
-    var audio1 = document.getElementById("buttercup-audio");
-    if (audio1.paused) audio1.play();
+    if (audio1.paused) { 
+        audio1.play()
+        audio2.pause()
+        audio3.pause()
+        audio4.pause()
+        audio5.pause()
+    }
     else audio1.pause();
 }
 
 function toggleAudio2() {
-    var audio2 = document.getElementById("soft-boy-audio");
-    if (audio2.paused) audio2.play();
+    if (audio2.paused) { 
+        audio1.pause()
+        audio2.play()
+        audio3.pause()
+        audio4.pause()
+        audio5.pause()
+    }
     else audio2.pause();
 }
 
 function toggleAudio3() {
-    var audio3 = document.getElementById("violet-audio");
-    if (audio3.paused) audio3.play();
+    if (audio3.paused) { 
+        audio1.pause()
+        audio2.pause()
+        audio3.play()
+        audio4.pause()
+        audio5.pause()
+    }
     else audio3.pause();
 }
 
 function toggleAudio4() {
-    var audio4 = document.getElementById("sorting-algorithms-audio");
-    if (audio4.paused) audio4.play();
+    if (audio4.paused) { 
+        audio1.pause()
+        audio2.pause()
+        audio3.pause()
+        audio4.play()
+        audio5.pause()
+    }
     else audio4.pause();
 }
 
 function toggleAudio5() {
-    var audio5 = document.getElementById("little-dark-age-audio");
-    if (audio5.paused) audio5.play();
+    if (audio5.paused) { 
+        audio1.pause()
+        audio2.pause()
+        audio3.pause()
+        audio4.pause()
+        audio5.play()
+    }
     else audio5.pause();
 }
