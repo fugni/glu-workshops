c73b270aa7b21357b953df6b2447ca64bd3c5c8e
Fungi 3 <sylvienuijtemans@gmail.com>
2022-11-11 14:58:06 +0100

    btiouhrjefsrho5erjewfqdw

--- module-3/workshop-1/assets/js/script.js
+++ module-3/workshop-1/assets/js/script.js
@@ -1,60 +1,43 @@
-var audio1 = document.getElementById("buttercup-audio");
-var audio2 = document.getElementById("soft-boy-audio");
-var audio3 = document.getElementById("violet-audio");
-var audio4 = document.getElementById("sorting-algorithms-audio");
-var audio5 = document.getElementById("little-dark-age-audio");
+const audioSrcArray = [
+    "buttercup.mp3",
+    "soft-boy.mp3",
+    "violet.mp3",
+    "sorting-algorithms.mp3",
+    "little-dark-age.mp3",
+];
 
-function toggleAudio1() {
-    if (audio1.paused) { 
-        audio1.play()
-        audio2.pause()
-        audio3.pause()
-        audio4.pause()
-        audio5.pause()
+const audioNameArray = [
+    "Buttercup - Jack Stauber",
+    "Soft Boy - Wilbur Soot",
+    "violet - vylt",
+    "sorting algorithms - AlphaGX",
+    "Little Dark Age - MGMT",
+];
+
+var currentAudio = 0;
+var audio = document.getElementById("audio");
+var nowPlaying = document.getElementById("now-playing");
+
+function nextAudio() {
+    currentAudio++;
+    if (currentAudio == 5) {
+        currentAudio = 0;
     }
-    else audio1.pause();
+    audio.setAttribute("src", "assets/audio/" + audioSrcArray[currentAudio]);
+    nowPlaying.textContent = audioNameArray[currentAudio];
 }
 
-function toggleAudio2() {
-    if (audio2.paused) { 
-        audio1.pause()
-        audio2.play()
-        audio3.pause()
-        audio4.pause()
-        audio5.pause()
+function previousAudio() {
+    currentAudio--;
+    if (currentAudio == -1) {
+        currentAudio = 4;
     }
-    else audio2.pause();
+    audio.setAttribute("src", "assets/audio/" + audioSrcArray[currentAudio]);
+    nowPlaying.textContent = audioNameArray[currentAudio];
 }
 
-function toggleAudio3() {
-    if (audio3.paused) { 
-        audio1.pause()
-        audio2.pause()
-        audio3.play()
-        audio4.pause()
-        audio5.pause()
-    }
-    else audio3.pause();
-}
-
-function toggleAudio4() {
-    if (audio4.paused) { 
-        audio1.pause()
-        audio2.pause()
-        audio3.pause()
-        audio4.play()
-        audio5.pause()
-    }
-    else audio4.pause();
-}
-
-function toggleAudio5() {
-    if (audio5.paused) { 
-        audio1.pause()
-        audio2.pause()
-        audio3.pause()
-        audio4.pause()
-        audio5.play()
-    }
-    else audio5.pause();
+function setAudio(a) {
+    currentAudio = a;
+    audio.setAttribute("src", "assets/audio/" + audioSrcArray[currentAudio]);
+    nowPlaying.textContent = audioNameArray[currentAudio];
 }
