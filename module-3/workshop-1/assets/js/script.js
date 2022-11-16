f57e4d24fa758e5bd3c36521012ee854a6fe2ff4
Fungi 3 <sylvienuijtemans@gmail.com>
2022-11-16 19:48:58 +0100

    aioesjrdhyudsifjodfubh

--- module-3/workshop-1/assets/js/script.js
+++ module-3/workshop-1/assets/js/script.js
@@ -18,63 +18,72 @@ let currentAudio = 0;
 const audio = document.getElementById("audio");
 const nowPlaying = document.getElementById("now-playing");
 
-const nextButton = document.querySelector(".next-button");
-const prevButton = document.querySelector(".previous-button");
-
 const track = document.querySelector(".carousel-track");
 const slides = Array.from(track.children);
 
-// sets slides next to eachother
-const slideWidth = slides[0].getBoundingClientRect().width;
-console.log(slideWidth);
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + "px";
-}
-slides.forEach(setSlidePosition);
+// sets slides next to eachother when page is loaded
+window.addEventListener("load", () => {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    const setSlidePosition = (slide, index) => {
+        slide.style.left = slideWidth * index + "px";
+    };
 
-// change slide function
+    slides.forEach(setSlidePosition);
+});
+
+// slide changer
 const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = "translateX(-" + targetSlide.style.left + ")";
     currentSlide.classList.remove("current-slide");
     targetSlide.classList.add("current-slide");
-}
+};
 
-// next audio
-nextButton.addEventListener("click", e => {
+// next slide & audio
+const nextButton = document.querySelector(".next-button");
+nextButton.addEventListener("click", () => {
     const currentSlide = track.querySelector(".current-slide");
     const nextSlide = currentSlide.nextElementSibling;
 
-    moveToSlide(track, currentSlide, nextSlide)
+    moveToSlide(track, currentSlide, nextSlide);
 
     currentAudio++;
     if (currentAudio == 5) {
         currentAudio = 0;
     }
+
     audio.setAttribute("src", "assets/audio/" + audioSrcArray[currentAudio]);
     nowPlaying.textContent = audioNameArray[currentAudio];
 });
 
-// previous audio
-prevButton.addEventListener("click", e => {
+// previous slide & audio
+const prevButton = document.querySelector(".previous-button");
+prevButton.addEventListener("click", () => {
     const currentSlide = track.querySelector(".current-slide");
     const prevSlide = currentSlide.previousElementSibling;
 
-    moveToSlide(track, currentSlide, prevSlide)
-
+    moveToSlide(track, currentSlide, prevSlide);
+    
     currentAudio--;
     if (currentAudio == -1) {
         currentAudio = 4;
     }
+
     audio.setAttribute("src", "assets/audio/" + audioSrcArray[currentAudio]);
     nowPlaying.textContent = audioNameArray[currentAudio];
 });
 
-function previousAudio() {
+// change slide & audio on list click
+const audioList = document.querySelector(".audio-list");
+const audioListItem = Array.from(audioList.children);
 
-}
+audioList.addEventListener("click", e => {
+    const targetAudio = e.target.closest("button");
 
-function setAudio(a) {
-    currentAudio = a;
-    audio.setAttribute("src", "assets/audio/" + audioSrcArray[currentAudio]);
-    nowPlaying.textContent = audioNameArray[currentAudio];
-}
+    console.log(targetAudio);
+})
+
+// function setAudio(a) {
+//     currentAudio = a;
+//     audio.setAttribute("src", "assets/audio/" + audioSrcArray[currentAudio]);
+//     nowPlaying.textContent = audioNameArray[currentAudio];
+// }
