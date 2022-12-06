33454007486abca7b6dbf456731b516719b153b4
fungi <sylvienuijtemans@gmail.com>
2022-12-06 11:17:40 +0100

    i dont want to doxx myself

---
+++ module-4/workshop-1/assets/js/script.js
@@ -0,0 +1,53 @@
+var voornaam = "Catra";
+var tussenvoegsel = "Applesauce";
+var achternaam = "Meowmeow";
+var volledigeNaam = voornaam + " " + tussenvoegsel + " " + achternaam
+var geboortedag = "1";
+var geboortemaand = "2";
+var geboortejaar = "2003";
+var huidig_jaartal = "2022"
+var leeftijd = 340;
+var dag_van_het_jaar = 340;
+var straat = "sus street";
+var huisnummer = "69";
+var woonplaats = "among us city";
+var woonachtig_in_utrecht = "False"; 
+var aantal_studenten_in_deze_klas = 21;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = aantal_studenten_in_deze_klas * kosten_van_frikandelbroodje; // bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+
+document.write("Voornaam: " + voornaam);
+document.write("<br>");
+document.write("Tussenvoegsel: " + tussenvoegsel);
+document.write("<br>");
+document.write("Achternaam: " + achternaam);
+document.write("<br>");
+document.write("Volledige naam: " + volledigeNaam);
+document.write("<br>");
+document.write("Geboortedag: " + geboortedag);
+document.write("<br>");
+document.write("Geboortemaand: " + geboortemaand);
+document.write("<br>");
+document.write("Geboortejaar: " + geboortejaar);
+document.write("<br>");
+document.write("Huidig jaartal: " + huidig_jaartal);
+document.write("<br>");
+document.write("Leeftijd: " + leeftijd)
+document.write("<br>");
+document.write("Dag van het jaar: " + dag_van_het_jaar)
+document.write("<br>");
+document.write("Straat: " + straat)
+document.write("<br>");
+document.write("Huisnummer: " + huisnummer);
+document.write("<br>");
+document.write("Woonplaats: " + woonplaats);
+document.write("<br>");
+document.write("Woonachtig in Utrecht: " + woonachtig_in_utrecht);
+document.write("<br>");
+document.write("Aantal studenten in deze klas: " + aantal_studenten_in_deze_klas);
+document.write("<br>");
+document.write("Kosten van frikandelbroodjes: " + kosten_van_frikandelbroodje);
+document.write("<br>");
+document.write("Totale lunchkosten: " + totale_lunchkosten);
+
+console.log("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀ ⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀ ⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀ ⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀ ⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀ ⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀ ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ ⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀ ⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀ ⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
