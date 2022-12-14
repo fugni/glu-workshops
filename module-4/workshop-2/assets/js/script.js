5627c12aa2280247b8eb9c71db66bb2e1ed0fcee
fungi <sylvienuijtemans@gmail.com>
2022-12-14 11:02:20 +0100

    piano but its shitty

---
+++ module-4/workshop-2/assets/js/script.js
@@ -0,0 +1,40 @@
+document.getElementById("a").onclick = function(){
+	var c_toets = document.getElementById("audio_a");
+	c_toets.play();
+}
+document.getElementById("b").onclick = function(){
+	var d_toets = document.getElementById("audio_b");
+	d_toets.play();
+}
+document.getElementById("c").onclick = function(){
+	var c_toets = document.getElementById("audio_c");
+	c_toets.play();
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+document.getElementById("f").onclick = function(){
+	var c_toets = document.getElementById("audio_f");
+	c_toets.play();
+}
+document.getElementById("g").onclick = function(){
+	var d_toets = document.getElementById("audio_g");
+	d_toets.play();
+}
+document.getElementById("h").onclick = function(){
+	var c_toets = document.getElementById("audio_h");
+	c_toets.play();
+}
+document.getElementById("i").onclick = function(){
+	var d_toets = document.getElementById("audio_i");
+	d_toets.play();
+}
+document.getElementById("j").onclick = function(){
+	var e_toets = document.getElementById("audio_j");
+	e_toets.play();
+}
